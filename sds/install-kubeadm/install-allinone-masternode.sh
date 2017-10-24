ab4e41af85ec72df63ed4fa5dfc67900671edc72
Hee Won Lee <knowpd@gmail.com>
2017-10-24 19:48:35 -0400

    updated

--- sds/install-kubeadm/install-allinone-masternode.sh
+++ sds/install-kubeadm/install-allinone-masternode.sh
@@ -11,5 +11,5 @@ sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f http://docs.projectcalico.org/v2.4/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
 kubectl taint nodes --all node-role.kubernetes.io/master-
