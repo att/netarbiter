410c4b69c25fc583e166d056a51cb6b8698fd3e9
Hee Won Lee <knowpd@gmail.com>
2017-10-25 10:38:08 -0400

    updated

--- sds/install-kubeadm/install-masternode.sh
+++ sds/install-kubeadm/install-masternode.sh
@@ -4,9 +4,24 @@
 
 set -x
 
-./install-docker
-./install-kubectl
-./install-kubelet-kubeadm 
+#./install-docker
+#./install-kubectl
+#./install-kubelet-kubeadm
+
+# Install docker
+sudo apt-get update
+sudo apt-get install -y docker.io
+
+# Install kubeadm, kubelet, and kubectl
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+
+# Initialize your master
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
