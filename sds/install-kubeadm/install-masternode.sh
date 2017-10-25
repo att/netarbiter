476565bdc9bd526d5bf331bbe9c0270e2b27bdc9
Hee Won Lee <knowpd@gmail.com>
2017-10-25 10:40:25 -0400

    updated

--- sds/install-kubeadm/install-masternode.sh
+++ sds/install-kubeadm/install-masternode.sh
@@ -26,5 +26,9 @@ sudo kubeadm init --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Install a pod network
 kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+
+# Schedule a pod on the master
 kubectl taint nodes --all node-role.kubernetes.io/master-
