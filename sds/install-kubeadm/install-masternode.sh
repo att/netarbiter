62bfd318620e93c7dd9d50b407e20010c777d5ff
Hee Won Lee <knowpd@gmail.com>
2017-10-25 16:16:21 -0400

    updated

--- sds/install-kubeadm/install-masternode.sh
+++ sds/install-kubeadm/install-masternode.sh
@@ -8,8 +8,8 @@ source common-functions.sh
 
 install_docker
 install_kubexxx 	# i.e., kubeadm, kubelet, and kubectl
-kubeadm_init_calico
-#kubeadm_init_flannel
+#kubeadm_init_calico
+kubeadm_init_flannel
 
 # Schedule a pod on the master
 kubectl taint nodes --all node-role.kubernetes.io/master-
