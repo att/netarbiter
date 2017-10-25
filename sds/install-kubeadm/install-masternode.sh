207301b8b06e23d07ad56cb80b517c22b8a41f4e
Hee Won Lee <knowpd@gmail.com>
2017-10-25 10:49:39 -0400

    updated

--- sds/install-kubeadm/install-masternode.sh
+++ sds/install-kubeadm/install-masternode.sh
@@ -13,13 +13,13 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 
 # Install kubeadm, kubelet, and kubectl
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo sh -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+EOF'
+sudo apt-get update
+sudo apt-get install -y --allow-unauthenticated kubeadm kubelet kubectl
 
 # Initialize your master
 sudo kubeadm init --pod-network-cidr=192.168.0.0/16
--- sds/install-kubeadm/install-workernode.sh
+++ sds/install-kubeadm/install-workernode.sh
@@ -4,5 +4,18 @@
 
 set -x
 
-./install-docker
-./install-kubelet-kubeadm 
+#./install-docker
+#./install-kubelet-kubeadm 
+
+# Install docker
+sudo apt-get update
+sudo apt-get install -y docker.io
+
+# Install kubeadm and kubelet
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo sh -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+sudo apt-get update
+sudo apt-get install -y --allow-unauthenticated kubeadm kubelet
