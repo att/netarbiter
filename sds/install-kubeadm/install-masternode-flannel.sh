13033ea23aa4fdb9a906f512dd7e32d501400720
Hee Won Lee <knowpd@gmail.com>
2017-10-25 16:27:46 -0400

    updated

---
+++ sds/install-kubeadm/install-masternode-flannel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Author: Hee Won Lee <knowpd@research.att.com>
+# Created on 10/20/2017
+
+set -x
+
+source common-functions.sh
+
+install_docker
+install_kubexxx 	# i.e., kubeadm, kubelet, and kubectl
+#kubeadm_init_calico
+kubeadm_init_flannel
+
+# Schedule a pod on the master
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
