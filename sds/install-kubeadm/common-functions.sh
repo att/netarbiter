b7e0f22ed3ac34aa0d50d35ff010c863590173de
Hee Won Lee <knowpd@gmail.com>
2017-10-25 15:50:00 -0400

    updated

--- sds/install-kubeadm/common-functions.sh
+++ sds/install-kubeadm/common-functions.sh
@@ -15,8 +15,8 @@ function install_kubexxx {
   sudo apt-get update && sudo apt-get install -y apt-transport-https
   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
   sudo sh -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-  deb http://apt.kubernetes.io/ kubernetes-xenial main
-  EOF'
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
   sudo apt-get update
   sudo apt-get install -y --allow-unauthenticated kubeadm kubelet kubectl
 
