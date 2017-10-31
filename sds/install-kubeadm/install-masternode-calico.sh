541419e277015b562f59ef96855a384d84157cdf
Hee Won Lee <knowpd@gmail.com>
2017-10-31 14:39:44 -0400

    updated

--- sds/install-kubeadm/install-masternode-calico.sh
+++ sds/install-kubeadm/install-masternode-calico.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Author: Hee Won Lee <knowpd@research.att.com>
 # Created on 10/20/2017
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <kubernetes-version>"
+    echo "  kubernetes-version:    e.g. latest, 1.7.6-00, 1.8.2-00, etc. "
+    exit 1
+fi
 
 set -x
+
+KUBERNETES_VERSION=$1
 
 source common-functions.sh
 
@@ -11,12 +18,14 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 
 # Install kubeadm, kubelet, and kubectl 
-install_kubexxx				# for latest version
+if [[ "$1" == "latest" ]]; then
+  install_kubexxx
+else
+  install_kubexxx $KUBERNETES_VERSION
+fi
 # Note:
 #   You can find a specific version from: 
 #   https://packages.cloud.google.com/apt/dists/kubernetes-xenial/main/binary-amd64/Packages
-#   Then, for instance, add the version as follows:
-#   install_kubexxx 1.7.6-00
 
 # kubeadm init and install calico
 kubeadm_init_calico
