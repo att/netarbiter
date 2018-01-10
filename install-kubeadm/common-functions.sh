f226bdd9add9a455fd40bd1b7a789db7b48ecd10
Hee Won Lee <knowpd@gmail.com>
2018-01-10 11:07:28 -0500

    updated

--- install-kubeadm/common-functions.sh
+++ install-kubeadm/common-functions.sh
@@ -33,6 +33,7 @@ function kubeadm_init_calico {
   
   # Install calico
   kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml
+  #kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
 }
 
 # Initialize your master with flannel
