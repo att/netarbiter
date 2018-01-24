a1955a33585317dd1bdddc53c31a48c9ce0d7145
Hee Won Lee <knowpd@gmail.com>
2018-01-24 14:57:43 -0500

    updated

--- nvmf/setup-nic-lossless-layer3.sh
+++ nvmf/setup-nic-lossless-layer3.sh
@@ -2,12 +2,15 @@
 # Author: Hee Won Lee <knowpd@research.att.com>
 # Created on 1/17/2017
 # Ref: https://community.mellanox.com/docs/DOC-2881
+
 if [[ "$#" -ne 2 ]]; then
     echo "Usage: $0 <interface> <device>"
     echo "  interface: e.g. eth4"
     echo "  device:    e.g. mlx5_2"
     exit 1
 fi
+
+set -x
 
 INTERFACE=$1
 DEVICE=$2
