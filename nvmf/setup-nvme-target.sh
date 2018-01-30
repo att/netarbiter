ef5bae7930a991f4b2d1572c75ae41a77941304b
Hee Won Lee <knowpd@gmail.com>
2018-01-30 14:48:28 -0500

    updated

--- nvmf/setup-nvme-target.sh
+++ nvmf/setup-nvme-target.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Author: Hee Won Lee <knowpd@research.att.com>
 # Created on 11/11/2017
+# Ref: https://community.mellanox.com/docs/DOC-2504
 
 set -x
 set -e
 
+TARGET_ADDRESS=10.154.0.61
+NVME_DEV=/dev/nvme0n1
 NVME_SUBSYSTEM_NAME=nvme-eris101
 NUMBER_OF_PORTS=1
 
@@ -15,17 +18,21 @@ sudo modprobe nvmet-rdma
 sudo modprobe nvme-rdma
 
 # NVMe Target Configuration
-sudo mkdir -p /sys/kernel/config/nvmet/subsystems/$NVME_SUBSYSTEM_NAME
-sudo bash -c "echo 1 > /sys/kernel/config/nvmet/subsystems/$NVME_SUBSYSTEM_NAME/attr_allow_any_host"
-sudo mkdir -p /sys/kernel/config/nvmet/subsystems/$NVME_SUBSYSTEM_NAME/namespaces/10
-sudo bash -c "echo -n /dev/nvme0n1> /sys/kernel/config/nvmet/subsystems/$NVME_SUBSYSTEM_NAME/namespaces/10/device_path"
-sudo bash -c "echo 1 > /sys/kernel/config/nvmet/subsystems/$NVME_SUBSYSTEM_NAME/namespaces/10/enable"
-sudo mkdir -p /sys/kernel/config/nvmet/ports/$NUMBER_OF_PORTS
-sudo bash -c "echo 10.154.0.61 > /sys/kernel/config/nvmet/ports/$NUMBER_OF_PORTS/addr_traddr"
-sudo bash -c "echo rdma > /sys/kernel/config/nvmet/ports/$NUMBER_OF_PORTS/addr_trtype"
-sudo bash -c "echo 4420 > /sys/kernel/config/nvmet/ports/$NUMBER_OF_PORTS/addr_trsvcid"
-sudo bash -c "echo ipv4 > /sys/kernel/config/nvmet/ports/$NUMBER_OF_PORTS/addr_adrfam"
-sudo ln -s /sys/kernel/config/nvmet/subsystems/$NVME_SUBSYSTEM_NAME /sys/kernel/config/nvmet/ports/$NUMBER_OF_PORTS/subsystems/$NVME_SUBSYSTEM_NAME
+SUBSYSTEM_PATH=/sys/kernel/config/nvmet/subsystems/$NVME_SUBSYSTEM_NAME
+sudo mkdir -p $SUBSYSTEM_PATH
+sudo bash -c "echo 1 > $SUBSYSTEM_PATH/attr_allow_any_host"
+sudo mkdir -p $SUBSYSTEM_PATH/namespaces/10
+sudo bash -c "echo -n $NVME_DEV > $SUBSYSTEM_PATH/namespaces/10/device_path"
+sudo bash -c "echo 1 > $SUBSYSTEM_PATH/namespaces/10/enable"
+
+PORT_PATH=/sys/kernel/config/nvmet/ports/$NUMBER_OF_PORTS
+sudo mkdir -p $PORT_PATH
+sudo bash -c "echo $TARGET_ADDRESS > $PORT_PATH/addr_traddr"
+sudo bash -c "echo rdma > $PORT_PATH/addr_trtype"
+sudo bash -c "echo 4420 > $PORT_PATH/addr_trsvcid"
+sudo bash -c "echo ipv4 > $PORT_PATH/addr_adrfam"
+
+sudo ln -s $SUBSYSTEM_PATH $PORT_PATH/subsystems/$NVME_SUBSYSTEM_NAME
 
 # Check
 dmesg | grep "enabling port"
