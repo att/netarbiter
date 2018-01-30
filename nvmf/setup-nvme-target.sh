cc0ffd012a6bd2c8047ba1febd9d56cfb0551a73
Hee Won Lee <knowpd@gmail.com>
2018-01-30 17:25:22 -0500

    updated

--- nvmf/setup-nvme-target.sh
+++ nvmf/setup-nvme-target.sh
@@ -3,13 +3,23 @@
 # Created on 11/11/2017
 # Ref: https://community.mellanox.com/docs/DOC-2504
 
+if [[ "$#" -ne 4 ]]; then
+    echo "Usage: $0 <target-address> <nvme-dev> <nvme-subsystem-name> <port-id>"
+    echo "  target-address:      e.g. 10.154.0.61"
+    echo "  nvme-dev:            e.g. /dev/nvme0n1"
+    echo "  nvme-subsystem-name: e.g. nvme-eris101"
+    echo "  port-id:             e.g. 1"
+    echo ""
+    exit 1
+fi
+
 set -x
 set -e
 
-TARGET_ADDRESS=10.154.0.61
-NVME_DEV=/dev/nvme0n1
-NVME_SUBSYSTEM_NAME=nvme-eris101
-NUMBER_OF_PORTS=1
+TARGET_ADDRESS=$1
+NVME_DEV=$2
+NVME_SUBSYSTEM_NAME=$3
+PORT_ID=$4
 
 # Prerequisites
 sudo modprobe mlx5_core
@@ -25,7 +35,7 @@ sudo mkdir -p $SUBSYSTEM_PATH/namespaces/10
 sudo bash -c "echo -n $NVME_DEV > $SUBSYSTEM_PATH/namespaces/10/device_path"
 sudo bash -c "echo 1 > $SUBSYSTEM_PATH/namespaces/10/enable"
 
-PORT_PATH=/sys/kernel/config/nvmet/ports/$NUMBER_OF_PORTS
+PORT_PATH=/sys/kernel/config/nvmet/ports/$PORT_ID
 sudo mkdir -p $PORT_PATH
 sudo bash -c "echo $TARGET_ADDRESS > $PORT_PATH/addr_traddr"
 sudo bash -c "echo rdma > $PORT_PATH/addr_trtype"
