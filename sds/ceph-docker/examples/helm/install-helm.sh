423758fc201e66a72f1b4ae8cd38e5214389a62e
Hee Won Lee <knowpd@gmail.com>
2018-05-25 16:16:44 -0400

    install-helm.sh: upgrade helm verion 2.6.1 to 2.9.1

--- sds/ceph-docker/examples/helm/install-helm.sh
+++ sds/ceph-docker/examples/helm/install-helm.sh
@@ -2,6 +2,6 @@
 # Author: Hee Won Lee <knowpd@research.att.com>
 # Created on 10/20/2017
 
-curl -O https://storage.googleapis.com/kubernetes-helm/helm-v2.6.1-linux-amd64.tar.gz
-tar xzvf helm-v2.6.1-linux-amd64.tar.gz 
+curl -O https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz
+tar xzvf helm-v2.9.1-linux-amd64.tar.gz 
 sudo cp linux-amd64/helm /usr/local/bin
